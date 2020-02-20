41a48a52d19c017402078c81165b74acb8c80b04
YusukeKyokawa <kiyo@YusukeKyokawanoMacBook-Pro.local>
2020-02-21 00:38:00 +0900

    push前

---
+++ Project/src/utils.py
@@ -0,0 +1,15 @@
+import os
+import json
+import pandas as pd
+import torch
+import numpy as np
+import random
+
+
+def seed_setting(seed=1029):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
