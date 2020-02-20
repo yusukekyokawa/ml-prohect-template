b6581d035d6b035f77fcfbba6c05dcd95fc415e3
YusukeKyokawa <kiyo@YusukeKyokawanoMacBook-Pro.local>
2020-02-21 00:05:26 +0900

    update

--- Project/experiments/exp0.py
+++ Project/experiments/exp0.py
@@ -1,10 +1,16 @@
 import torch
 import hydra
 import yaml
-
+imprt 
 
 def main():
+
+    now = datetime.datetime.now()
+    now_date = '{}-{:0>2d}-{:0>2d}_{:0>2d}-{:0>2d}-{:0>2d}'.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+    print('{}-{}-{} {}:{}:{}'.format(now.year, now.month, now.day, now.hour, now.minute, now.second))
     #### パラメータの設定 ###
     with open('../params/param.yaml', "r+") as f:
         param = yaml.load(f, Loader=yaml.FullLoader)
+
+
     
