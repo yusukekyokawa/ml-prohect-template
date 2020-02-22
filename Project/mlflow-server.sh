e0e9a02ada6099686285a71f274fd7909c32f3b6
yusukekyokawa <medakasuisui598@gmail.com>
2020-02-22 12:07:12 +0900

    docker-compose修正

--- Project/mlflow-server.sh
+++ Project/mlflow-server.sh
@@ -7,6 +7,6 @@ EOF
 
 #    --backend-store-uri ${PATH_TO_TRACKING}/myruns \
 exec mlflow server \
-    --backend-store-uri ../Project/mlruns \
+    --backend-store-uri ../mlruns \
     --default-artifact-root ../artifact \
     --host 0.0.0.0
